16275992c2595c521506fe69208fae88bbf5735c
enom2g <enom2g@gmail.com>
2019-11-27 17:28:25 +0900

    Create project

---
+++ spring-webservice/src/main/java/com/enom2g/webservice/Application.java
@@ -0,0 +1,12 @@
+package com.enom2g.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring-webservice/src/test/java/com/enom2g/springwebservice/webserviceTests.java
@@ -0,0 +1,13 @@
+package com.enom2g.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class webserviceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
